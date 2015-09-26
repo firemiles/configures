92d6eba12094263cb926b1ece6cd45c5a9e36387
firemiles <firemiles@163.com>
2015-09-26 18:55:59 +0800

    fix configure file name

--- linux/install-develop-env.sh
+++ linux/install-develop-env.sh
@@ -7,12 +7,12 @@ sudo apt-get -y install git ipython vim zsh tmux
 git config --global core.editor vim
 
 # clone configure
-git clone https://github.com/firemiles/configures.git ~/configure
-cd ~/configure; git submodule update --init comm/zsh/z; git submodule update --init comm/vim/vim/bundle/Vundle.vim
+git clone https://github.com/firemiles/configures.git ~/configures
+cd ~/configures; git submodule update --init comm/zsh/z; git submodule update --init comm/vim/vim/bundle/Vundle.vim
 
 # configure vim
-ln -s /home/$USER/configure/comm/vim/vim ~/.vim
-ln -s /home/$USER/configure/comm/vim/vimrc ~/.vimrc
+ln -s /home/$USER/configures/comm/vim/vim ~/.vim
+ln -s /home/$USER/configures/comm/vim/vimrc ~/.vimrc
 
 # add powerline-font
 git clone https://github.com/powerline/fonts.git /tmp/powerline-font && sh /tmp/powerline-font/install.sh
@@ -20,8 +20,8 @@ git clone https://github.com/powerline/fonts.git /tmp/powerline-font && sh /tmp/
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo 'zsh' >> ~/.bashrc
-ln -s /home/$USER/configure/comm/zsh/robbyrussell-firemiles.zsh-theme ~/.oh-my-zsh/themes/robbyrussell-firemiles.zsh-theme 
-ln -s /home/$USER/configure/comm/zsh/z ~/.oh-my-zsh/custom/plugins/z 
+ln -s /home/$USER/configures/comm/zsh/robbyrussell-firemiles.zsh-theme ~/.oh-my-zsh/themes/robbyrussell-firemiles.zsh-theme 
+ln -s /home/$USER/configures/comm/zsh/z ~/.oh-my-zsh/custom/plugins/z 
 
 # install docker
 if [ $(getconf WORD_BIT) = '32' ] && [ $(getconf LONG_BIT) = '64' ] ; then # x86_64
