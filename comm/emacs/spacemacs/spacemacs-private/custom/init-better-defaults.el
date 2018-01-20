acbb966978755bda7e17c885d2dda7205c4c4439
firemiles <miles.dev@outlook.com>
2018-01-20 15:03:09 +0800

    remove chinese layer and set font

--- comm/emacs/spacemacs/spacemacs-private/custom/init-better-defaults.el
+++ comm/emacs/spacemacs/spacemacs-private/custom/init-better-defaults.el
@@ -1,8 +1,18 @@
+(provide 'init-better-defaults)
+
 (global-visual-line-mode 1)
 (global-linum-mode 1)
 
 ;; setting font
-;; (spacemacs//set-monospaced-font "Menlo" "PingFang" 13 15)
+;; (spacemacs//set-monospaced-font "Menlo" "PingFang" 13 15) ;; need chinese layer
+
+(setq chinese-font "Consolas")
+(cond ((eq system-type 'darwin) (setq chinese-font "PingFang SC"))
+       ((eq system-type 'window-nt) (setq chinese-font "Microsoft Yahei")))
+
+(dolist (charset '(kana han cjk-misc bopomofo))
+  (set-fontset-font (frame-parameter nil 'font) charset
+                    (font-spec :family chinese-font :size 20)))
 
 ;; setting time stamp format to English
 (setq system-time-locale "C")
@@ -19,4 +29,3 @@
   (goto-char (point-min))
   (while (search-forward "\r" nil t) (replace-match "")))
 
-(provide 'init-better-defaults)
